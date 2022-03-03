c5de05b2b25ed4dd13a3c82347364bf59063847c
alane95 <faydeanna1@gmail.com>
2022-03-02 22:00:03 -0500

    create button in html and select them in js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("it's working")
+console.log("it's workin");
+
+
+const feedEL = document.getElementById("food");
+const playEL = document.getElementById("game");
+const sleepEL = document.getElementById("Sleepy");
+// console.log(feedEL);
+
+
+feedEL.addEventListener('click', fed);
+playEL.addEventListener('click', myplay);
+sleepEL.addEventListener('click', slept);
+
+
+
+
