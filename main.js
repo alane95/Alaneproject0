3d809180d34695a96dfef7ac14446070d855b3bc
alane95 <faydeanna1@gmail.com>
2022-03-03 16:36:10 -0500

    I selected my button

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ const feedEL = document.getElementById("food");
 const playEL = document.getElementById("game");
 const sleepEL = document.getElementById("sleepy");
 // console.log(feedEL);
+const myCountO = document.getElementById("count-1")
+const myCountT= document.getElementById("count-2")
+const myCountH = document.getElementById("count-3")
+// console.log("hi");
+let num= 0;
+// my counter function for hunger
+myCountO.innerText= num;
+console.log(myCountO.innerText)
+
+// myCountT.innerText=num;
+// myCountH.innerText=num;
+
 
 const food=()=>{
     console.log(food);
