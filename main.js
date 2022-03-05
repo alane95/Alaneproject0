b38ba3b326b4ecd608f72c55bbeaf5f18b4bcc3e
alane95 <faydeanna1@gmail.com>
2022-03-04 19:50:26 -0500

    clear interval is working

--- main.js
+++ main.js
@@ -1,41 +1,75 @@
-console.log("it's workin");
-
-
-const feedEL = document.getElementById("food");
-const playEL = document.getElementById("game");
-const sleepEL = document.getElementById("sleepy");
+const feedEl = document.getElementById("feed-btn");
+const playEl = document.getElementById("play-btn");
+const sleepEl = document.getElementById("sleep-btn");
 // console.log(feedEL);
-const myCountO = document.getElementById("count-1")
-const myCountT= document.getElementById("count-2")
-const myCountH = document.getElementById("count-3")
+const myCountO = document.getElementById("feed-count")
+// console.log (myCountO)
+const myCountT= document.getElementById("play-count")
+const myCountH = document.getElementById("sleep-count")
 // console.log("hi");
+const startButton = document.getElementById("start-button")
+console.log(startButton);
 let num= 0;
-let counter = null;
+let myCount = null;
 
 // console.log(myCountO.innerText)
 
 // myCountT.innerText=num;
 // myCountH.innerText=num;
+// let intervalFunc = setInterval(function(){
+//   num++;
+//   console.log(num);
+// }, 1000);
+let started = false;
 
-
-function pFood(){
-    // console.log(pFood);
-    counter = setInterval(function(){
-    num++;
-    myCountO.innerText= num;
-},1000);
-    // console.log(num)
+function buttonStart() {
+  if(!started){
+    myCount = setInterval(function(){
+      num++;
+      stopMyCount()
+      myCountO.innerText= num;
+      myCountT.innerText= num;
+      myCountH.innerText= num;
+      started = true;
+    },1000);
+  }
 }
+function pFood(){
+   if (num >1 ){
+      num--
+    myCountO.innerText= num;
+   }
+}
+function stopMyCount() {
+  if (num >= 10){
+    clearInterval(myCount)
+  
+    console.log("hi");
+  
+  }
+}
+
+  
 const myplay=()=>{
-    console.log(myplay);
+    // console.log(myplay);
+   if (num >1 ){
+      num--
+    myCountO.innerText= num;
+   }
 }
 const dosleep=()=>{
-    console.log(dosleep);
+ if (num >1 ){
+      num--
+    myCountO.innerText= num;
+   }
+ 
 }
 
-feedEL.addEventListener('click', pFood);
-playEL.addEventListener('click', myplay);
-sleepEL.addEventListener('click', dosleep);
+feedEl.addEventListener('click', pFood);
+playEl.addEventListener('click', myplay);
+sleepEl.addEventListener('click', dosleep);
+startButton.addEventListener('click', buttonStart);
+
 
 
 
