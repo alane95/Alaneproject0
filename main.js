7d336c5aa3c23025e531c0bf196920e9aa4cd0c7
alane95 <faydeanna1@gmail.com>
2022-03-06 09:05:10 -0500

    change background color on click

--- main.js
+++ main.js
@@ -2,24 +2,17 @@ const feedEl = document.getElementById("feed-btn");
 const playEl = document.getElementById("play-btn");
 const sleepEl = document.getElementById("sleep-btn");
 // console.log(feedEL);
-const myCountO = document.getElementById("feed-count")
+const myCountO = document.getElementById("feed-count");
 // console.log (myCountO)
-const myCountT= document.getElementById("play-count")
-const myCountH = document.getElementById("sleep-count")
+const myCountT = document.getElementById("play-count");
+const myCountH = document.getElementById("sleep-count");
 // console.log("hi");
-const startButton = document.getElementById("start-button")
-console.log(startButton);
+const startButton = document.getElementById("start-button");
+// console.log(startButton);
+const myAgeCount = document.getElementById("age-count");
+
 let num= 0;
 let myCount = null;
-
-// console.log(myCountO.innerText)
-
-// myCountT.innerText=num;
-// myCountH.innerText=num;
-// let intervalFunc = setInterval(function(){
-//   num++;
-//   console.log(num);
-// }, 1000);
 let started = false;
 
 function buttonStart() {
@@ -30,22 +23,25 @@ function buttonStart() {
       myCountO.innerText="Hunger " + num;
       myCountT.innerText="Boredom " + num;
       myCountH.innerText="Sleepiness " + num;
+    //   myAgeCount.innerText="Age " + num;
       started = true;
     },2000);
   }
 }
-function pFood(){
-   if (num >1 ){
-      num--
-    myCountO.innerText= "Hunger " + num;
-   }
-   if  (document.getElementById("imgClickAndChange").src == "https://i.pinimg.com/originals/ae/a5/50/aea5503452ac0387b5fea012197e56cb.gif") 
+
+function pFood(body,color){
+    document.body.style.backgroundColor = '#70bfb9';
+    if  (document.getElementById("imgClickAndChange").src == "https://i.pinimg.com/originals/ae/a5/50/aea5503452ac0387b5fea012197e56cb.gif") 
    {
        document.getElementById("imgClickAndChange").src = "https://i.pinimg.com/originals/55/5c/39/555c39d22c91d954f2892e99bb71e265.gif";
    }
-
-   
+   if (num >1 ){
+    num--
+  myCountO.innerText= "Hunger " + num;
+ }
+ 
 }
+
 const myplay=()=>{
     // console.log(myplay);
    if (num >1 ){
@@ -82,11 +78,20 @@ function stopMyCount() {
     }
 }
 
+
+
+
 feedEl.addEventListener('click', pFood);
 playEl.addEventListener('click', myplay);
 sleepEl.addEventListener('click', dosleep);
 startButton.addEventListener('click', buttonStart);
 
+
+function myButton(){
+    let myText = document.getElementById("myText");
+    let myMessage = document.getElementById("message");
+    myMessage.innerHTML = "welcome to your tamagotchi " + myText.value;
+}
 
 
 
